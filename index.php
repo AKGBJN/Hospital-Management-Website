a728b1c303bb45927f14868a5fa60a9502eb60a0
Sangeeta <70089298+Sangee2507@users.noreply.github.com>
2021-10-25 22:23:50 +0530

    Update index.php
    Please review the corrections, I have made in the file.
    It has some functions, missing in the PHP script.
    
    Thanks

--- index.php
+++ index.php
@@ -12,7 +12,23 @@ session_start();
  	<h1>Welcome to Projectworlds Hospital Management System</h1>
     <p class="block-quote">Our aim has always been to bring world–class medical care within the reach of common man.</p>
     <p><?php include('slideshow.php');?></p>
-  
+  <?php 
+    if(isset($_POST['lemail'])){
+      $i = login($_POST['lemail'],$_POST['lpassword'],"users");
+      if($i==1){
+        echo '<script type="text/javascript"> window.location = "add_patient.php" </script>';
+      }
+    }else if(isset($_POST['remail'])){
+      $i = register($_POST['remail'],$_POST['rpassword'],$_POST['rfullname'],"users");
+      if($i==1){
+        echo '<script type="text/javascript"> window.location = "add_patient.php"</script>';
+      }
+    }else{
+      echo "<div class='alert alert-info'>
+              <strong>Info!</strong> Login or Register to be able to book your appointment.</div>";
+    }
+    unset($_POST);
+  ?>
 <div class="row">
   <div class="col col-xl-6 col-sm-6">
       <h2>Login</h2>
